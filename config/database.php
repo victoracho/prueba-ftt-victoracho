4f8233afbcd0202a284864c501b9754c960c3995
victoracho <32299455+victoracho@users.noreply.github.com>
2022-02-19 09:25:03 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'id18458827_talento'),
+            'username' => env('DB_USERNAME', 'id18458827_victoracho'),
+            'password' => env('DB_PASSWORD', '8?j>6[|hFSc-bCjr'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
